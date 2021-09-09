aa15a396c723589404b53b03110f222485ab8684
Zaki Shaheen <zakishaheen@users.noreply.github.com>
2021-09-09 16:55:12 -0700

    wireup ignore-regex

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const exec = require('@actions/exec')
 async function main() {
     const derivedDataPath = core.getInput('derived-data-path')
     const target = core.getInput('target')
+    const ignoreRegex = core.getInput('ignore-regex')
     const format = core.getInput('file-format')
     const output = core.getInput('output-file')
-    await exec.exec(`${__dirname}/cov.sh -derivedDataPath ${derivedDataPath} -target ${target} -format ${format} -output ${output}`)
+    await exec.exec(`${__dirname}/cov.sh -derivedDataPath ${derivedDataPath} -target ${target} -format ${format} -ignore-regex ${ignoreRegex} -output ${output}`)
 }
 
 main().catch(err => core.setFailed(err.message))
